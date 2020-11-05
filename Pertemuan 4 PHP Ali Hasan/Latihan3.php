ab38c7c757129c29035117147c7986b3071c8988
aioliaa <hasanmuhammadali8@gmail.com>
2020-11-05 16:09:26 +0800

    Crud

--- Pertemuan 4 PHP Ali Hasan/Latihan3.php
+++ Pertemuan 4 PHP Ali Hasan/Latihan3.php
@@ -4,26 +4,67 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Get and Post</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-	<form action="" method="post">
-		Id <input type="number" name="id"><br>
-		Nama <input type="text" name="nama"><br>
-		Umur <input type="text" name="umur"><br>
-		<button type="submit" name="submit">Submit</button>
-	</form>
+	<div class="container">
+		<div class="col">
+			<div class="cols-12">
+				<h4>Data Jam Belajar</h4>
+				<hr>
+				<form action="" method="POST">
+					<div class="form-group">
+					<label for="usia">Usia</label>
+					<input type="text" name="usia" id="usia" class="form-control" required>
+					</div>
+					<div class="form-group float-right">
+                    <a  name="submit" class="btn btn-success btn-md">Submit</a>
+                   </div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
 	
 </body>
 </html>
 
 <?php 
-if (isset($_POST['submit'])) {
-	$id = $_POST ['id'];
-	$nama = $_POST ['nama'];
-	$umur = $_POST ['umur'];
+if (isset($POST['submit'])) {
+	$usia = $_POST['usia'];
 
-	echo "ID Anda : ".$id;
-	echo "<br>Nama Anda : ".$nama;
-	echo "<br>Umur Anda : ".$umur;
+    if ($usia < 6 ) {
+        $ket = 'B';
+    } elseif ($usia < 17) {
+        $ket = 'A';
+    } elseif ($usia < 51) {
+        $ket = 'D';
+    }else{
+        $ket = 'T';
+    }
+
+    switch (true) {
+        case ($ket = 'B'):
+            $status = 'Balita';
+            break;
+
+        case ($ket = 'A'):
+            $status = 'Anak-anak';
+            break;
+
+        case ($ket = 'D'):
+            $status = 'Dewasa';
+            break;
+        
+        default:
+            $status = 'Tua';
+            break;
+    
+    
+         echo "Usia Anda : ".$usia;
+         echo "Data Gagal ditambahkan";
+  }
 }
-?>
+ ?>
